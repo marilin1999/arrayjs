678fbd168614b685640cef53b013fa70023de80c
marilin1999 <140349328+marilin1999@users.noreply.github.com>
2023-08-24 18:52:27 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,10 @@
+
+let Myarray = [2 , "marilin", 24, false]
+Myarray[0] = "juanse"
+Myarray[1] = 20
+  
+let variable = "6 - marilin - 24 - false"
+
+console.log(Myarray[1])
+console.log(Myarray)
+console.log(variable)
